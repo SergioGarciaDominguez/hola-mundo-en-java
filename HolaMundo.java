8095f8a5499291546171e4d75d43a4bc033a6de8
Sergio García Domínguez <sergio_gd92@hotmail.com>
2016-10-05 09:33:37 +0200

    Añadidos colores al HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,14 @@
+/**
+* Hola mundo en java con colores
+*
+* @author Sergio García Domínguez
+*/
+
 public class HolaMundo{
   public static void main(String[] args) {
     String naranja = "\033[33m";
+    String azul = "\033[34m";
     
-    System.out.println("Hola" + naranja + " Mundo");
+    System.out.println(azul + "Hola" + naranja + " Mundo");
   }
 }
